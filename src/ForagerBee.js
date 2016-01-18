51db01eba40f8112f30a6008fe4f76f30d7b6fcd
Jonathon Lo <jonathon.lo35@gmail.com>
2016-01-18 15:45:52 -0800

    'added taxonomy'

---
+++ src/ForagerBee.js
@@ -0,0 +1,17 @@
+var ForagerBee = function() {
+
+  Bee.call(this);
+  this.age = 10;
+  this.canFly = true;
+  this.treasureChest = [];
+  this.job = 'find pollen';
+
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+
